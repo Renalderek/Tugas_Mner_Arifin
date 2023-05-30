5a59bc787c86c5246aff8c41eb2c976dcde22341
renalderek <derekreinalto@gmail.com>
2023-05-30 09:29:42 +0800

    bagian keamanan

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,11 +4,15 @@ import CryptocurrencySection from "../components/Section/CryptocurrencySection";
 import Footer from "../components/Footer";
 import FaqSection from "../components/Section/FaqSection";
 import BackToTopSection from "../components/Section/BackToTopSection";
+import SecuritySection from "../components/Section/SecuritySection";
+import StepSection from "../components/Section/StepSection";
 export default function Index() {
   return (
     <Layout>
       <HeroSection />
       <CryptocurrencySection />
+      <SecuritySection />
+      <StepSection />
       <FaqSection />
       <BackToTopSection />
       <Footer />
