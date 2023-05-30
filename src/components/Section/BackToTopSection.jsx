5c3dafdd5fb4e724f98fe6e38a68c314093164a7
renalderek <derekreinalto@gmail.com>
2023-05-30 08:27:22 +0800

    bryan punya mar taso kase kelar

---
+++ src/components/Section/BackToTopSection.jsx
@@ -0,0 +1,11 @@
+export default function BackToTopSection() {
+  return (
+    <section>
+      <div className="container mx-auto text-center mb-5">
+        <button className="border border-[#DDDDDD] bg-[#FAFAFA] rounded-xl py-4 px-6">
+          Back to top
+        </button>
+      </div>
+    </section>
+  );
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,11 +2,15 @@ import Layout from "../components/Layout";
 import HeroSection from "../components/Section/HeroSection";
 import CryptocurrencySection from "../components/Section/CryptocurrencySection";
 import Footer from "../components/Footer";
+import FaqSection from "../components/Section/FaqSection";
+import BackToTopSection from "../components/Section/BackToTopSection";
 export default function Index() {
   return (
     <Layout>
       <HeroSection />
       <CryptocurrencySection />
+      <FaqSection />
+      <BackToTopSection />
       <Footer />
     </Layout>
   );
